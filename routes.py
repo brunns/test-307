25b42c23493bd225fe4e5bcfc575ee0c4fd6b94b
Simon Brunning <simon.brunning@mnscorp.net>
2016-05-06 12:02:58 +0100

    Show form result.

--- routes.py
+++ routes.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, session
 import logging
+import random
+import string
 import sys
 import warnings
 
+logger = logging.getLogger(__name__)
+
 app = Flask(__name__) 
 app.debug = True
-logger = logging.getLogger(__name__)
+app.secret_key = ''.join(random.choice(string.ascii_letters) for _ in range(100))
 
 @app.route('/') 
 def hello(): 
-    logger.info("hello")
-    return "Hello World."
+    logger.info("root")
+    return '<http><body><p>Hello %s</p><p><a href="/form">Form here.</a></p></body></http>' % session.get('foo', 'World')
     
 @app.route('/form')
 def form():
+    logger.info("form")
     return '<http><body><form method="POST" action="/first"><input type="text" name="foo"/><input type="submit"></form></body></http>'
 
 @app.route('/first', methods=['POST'])
 def first():
     """Trigger with:
     $ http -f --follow POST http://0.0.0.0:5000/first foo="bar"
+    
+    Or with form.
     """
     foo = request.form['foo']
     logger.info("foo: %s", foo)
@@ -32,6 +39,7 @@ def first():
 def second():
     foo = request.form['foo']
     logger.info("foo: %s", foo)
+    session['foo'] = foo
     return redirect('/')
 
 @app.before_first_request
